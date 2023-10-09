12068e3a11bb76aa31f21d8f05fc525d85386eae
Lakshay Wadhwa <lakshaywadhwa1993@gmail.com>
2023-10-09 19:30:52 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,8 +19,8 @@ def get_requirements(file_path:str)->List[str]:
 setup(
 name='mlproject',
 version='0.0.1',
-author='Krish',
-author_email='krishnaik06@gmail.com',
+author='Lakshay',
+author_email='lakshaywadhwa261992@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
 
